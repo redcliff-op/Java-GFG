4ac4ee263925b52b75a4bac6a3cbd3d5ef7111e8
redcliff-op <devyashjangid@gmail.com>
2022-09-27 18:04:23 +0530

    1D-Array-Programs: Sort an Array

---
+++ Code/1D-Array-Programs/Sort an Array/Code.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Code {
+    public static void main(String[] args) {
+        int [] arr = {-5,-66,69,6,3,88,456,22};
+        Arrays.sort(arr);
+        for(int e: arr){
+            System.out.print(e + " ");
+        }
+    }    
+}
