0099cf335fafe31c6e6c79b738602098a9a976f0
redcliff-op <devyashjangid@gmail.com>
2022-10-16 17:06:32 +0530

    Exception-Programs: Unreachable Code Error

---
+++ Code/Exception-Programs/Unreachable Code Error/Code.java
@@ -0,0 +1,9 @@
+public class Code{
+    public static String xd(){
+        return "Hello There";
+        System.out.println("Hi");
+    }
+    public static void main(String[] args) {
+        xd();
+    }
+}
