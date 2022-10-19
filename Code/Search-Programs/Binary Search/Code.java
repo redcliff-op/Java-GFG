cee5ff2da860db4fe2d660b40d63a465491733cc
nasir-buddy <syedali172000@gmail.com>
2022-10-20 00:58:57 +0530

    Array : make binary search more understandable

--- Code/Search-Programs/Binary Search/Code.java
+++ Code/Search-Programs/Binary Search/Code.java
@@ -1,23 +1,49 @@
-public class Code{
-    public static int BSearch(int arr[], int x){
+import java.util.Scanner;
+
+public class Code {
+    static Scanner sc = new Scanner(System.in);
+
+    public static int BSearch(int arr[], int elem) {
         int low = 0;
-        int high = arr.length-1;
-        while (low<=high){
-            int mid = (high+low)/2;
-            if(x==arr[mid]){
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            if (elem == arr[mid]) {
                 return mid;
-            }
-            else if (x<arr[mid]){
+            } else if (elem < arr[mid]) {
                 high = mid - 1;
-            }
-            else{
+            } else {
                 low = mid + 1;
             }
         }
         return -1;
     }
+
+    public static int[] takeinput() {
+        int size = sc.nextInt();
+        int input[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            input[i] = sc.nextInt();
+        }
+        return input;
+    }
+
+    public static void print(int input[]) {
+        int size = input.length;
+        for (int i = 0; i < size; i++) {
+            System.out.print(input[i] + " ");
+
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        int [] test = {-24,-10,-1,1,3,4,5,69,77,89,101};
-        System.out.print(BSearch(test, 69));
+        int arr[] = takeinput();
+        System.out.print("Printing the default array.... : ");
+        print(arr);
+        System.out.print("Enter the element that you want to find : ");
+        int element = sc.nextInt();
+        int output = BSearch(arr, element);
+        System.out.println(output);
     }
 }
