0229039c70a634096485fecdfc5f8bbcbf7f2f67
redcliff-op <devyashjangid@gmail.com>
2023-05-28 12:23:10 +0530

    [Simplify] Misc-Problems: Advance Ladder

--- Code/Misc-Problems/Advance Ladder Pattern/Code.java
+++ Code/Misc-Problems/Advance Ladder Pattern/Code.java
@@ -1,35 +1,32 @@
+import java.util.Scanner;
+
 public class Code{
     public static void main(String[] args) {
-        int x = 6;
-        for(int i=0;i<=x;i++){
-            if(i<x-1){
-                System.out.println("@");
-            }else if(i==x){
-                System.out.print("@");
-                for(int j=0;j<x;j++){
-                    System.out.print("*");
+        int n = 6;
+        for(int i=0;i<n;i++){
+            System.out.println();
+            System.out.print("@");
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<(n*i);j++){
+                if(j==0){
+                    System.out.print(" ");
                 }
+                System.out.print(" ");
+            }
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            if(!(i==(n-1))){
                 System.out.println();
-                for(int s=0,t=x;s<x-1;s++,t=t+x){
-                    for(int y=0;y<=t;y++){
+            }
+            if(i==(n-1)){
+                System.out.println("@");
+                for(int k=1;k<n;k++){
+                    for(int l=0;l<=(i*(n+1)+1);l++){
                         System.out.print(" ");
                     }
-                    for(int q=0;q<x;q++){
-                        System.out.print("*");
-                    }
-                    if(s==(x-2)){
-                        for(int r=0;r<x;r++){
-                            if(r==0){
-                                System.out.print("@");
-                            }else{
-                                for(int p=0;p<=(x*x);p++){
-                                    System.out.print(" ");
-                                }
-                                System.out.print("@");   
-                            }
-                            System.out.println();
-                        }   
-                    }
+                    System.out.print("@");
                     System.out.println();
                 }
             }
